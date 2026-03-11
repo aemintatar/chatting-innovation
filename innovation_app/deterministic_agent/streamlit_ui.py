a4fb35b595124f7267889dcd93eabd99e96bf9ba
aemintatar <emintatar@hotmail.com>
2026-03-11 13:53:05 +0100

    summary

--- innovation_app/deterministic_agent/streamlit_ui.py
+++ innovation_app/deterministic_agent/streamlit_ui.py
@@ -213,7 +213,7 @@ if st.session_state.get('selected_codes'):
 # 🔹 STEP 4: Summarize & Download
 # =========================
 
-if len(st.session_state.get("selected_docs")) > 0:
+if len(st.session_state.get("selected_docs",[])) > 0 and len(st.session_state.get("selected_docs",[])) < 6:
 
     st.markdown("### Summarize selected documents")
     st.write("You can generate summaries for the selected documents.")
